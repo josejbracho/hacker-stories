fbae27aae142c3ca72c02892540660f7498697c5
jbracho <josejbracho@gmail.com>
2021-09-13 17:58:42 -0400

    update

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = React.useState('React');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -34,7 +34,7 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch}/>
+      <Search search={searchTerm} onSearch={handleSearch}/>
 
       <hr />
 
@@ -68,7 +68,12 @@ const Item = (props) => (
 const Search = (props) => (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch} />
+      <input 
+        id="search" 
+        type="text"
+        value={[props.search]} 
+        onChange={props.onSearch} 
+      />
     </div>
 );
 
