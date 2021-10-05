7d8bd250e0e9948cf20e28325a81bde118cc2417
jbracho <josejbracho@gmail.com>
2021-10-05 06:52:14 -0400

    update

--- src/App.js
+++ src/App.js
@@ -63,21 +63,18 @@ const App = () => {
     { data: [], isLoading: false, isError: false}
   );
 
-  const handleFetchStories = React.useCallback(() => {
-
-      dispatchStories({ type: 'STORIES_FETCH_INIT' });
-
-    axios
-      .get(url)
-      .then((result) => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits,
-        });
-      })
-      .catch(() => 
-        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-      );
+  const handleFetchStories = React.useCallback(async () => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+    try {
+      const result = await axios.get(url);
+      
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories( { type: 'STORIES_FETCH_FAILURE' });
+    }
   }, [url]);
 
   React.useEffect(() => {
