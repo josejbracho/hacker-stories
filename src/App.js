fabfa9ba1daf626b0b67d616420bab36eb04c81f
jbracho <josejbracho@gmail.com>
2021-10-08 06:34:18 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,84 +1,9 @@
 import * as React from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
 
-const StyledContainer = styled.div`
-  height: 100vw;
-  padding: 20px;
-  background: #83a4d4;
-  background: linear-gradient(to left, #b6fbff, #83a4d4);
-
-  color: #171212;
-`;
-
-const StyledHeadlinePrimary = styled.h1`
-  font-size: 48px;
-  font-weight: 300;
-  letter-spacing: 2px;
-`;
-
-const StyledItem = styled.li`
-  display: flex;
-  aling-items: center;
-  padding-bottom: 5px;
-`;
-
-const StyledColumn = styled.span`
-  padding: 0 5px;
-  white-space: nowrap;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-
-  a {
-    color: inherit;
-  }
-
-  width: ${(props) => props.width};
-`;
-
-const StyledButton = styled.button`
-  background: transparent;
-  border: 1px solid #171212;
-  padding: 5px;
-  cursor: pointer;
-
-  transition: all 0.1s ease-in;
-
-  &:hover {
-    background: #171212;
-    color: #ffffff;
-  }
-`;
-
-const StyledButtonSmall = styled(StyledButton)`
-  padding: 5px;
-`;
-
-const StyledButtonLarge = styled(StyledButton)`
-  padding: 10px;
-`;
-
-const StyledSearchForm = styled.form`
-  padding: 10px 0 20px 0;
-  display: flex;
-  align-items: baseline;
-`;
-
-const StyledLabel = styled.label`
-  border-top: 1px solid #171212;
-  border-left: 1px solid #171212;
-  padding-left: 5px;
-  font-size: 24px;
-`;
-
-const StyledInput = styled.input`
-  border: none;
-  border-bottom: 1px solid #171212;
-  background-color: transparent;
-
-  font-size: 24px;
-`;
+import './App.css';
+import { BiSearchAlt } from 'react-icons/bi'
+import { ImCheckmark2 } from 'react-icons/im'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -179,8 +104,8 @@ const App = () => {
   };
 
   return (
-    <StyledContainer>
-      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -195,7 +120,7 @@ const App = () => {
       ) : (
       <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
-    </StyledContainer>
+    </div>
   );
 };
 
@@ -204,7 +129,7 @@ const SearchForm = ({
   onSearchInput,
   onSearchSubmit,
 }) => (
-  <StyledSearchForm onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -214,11 +139,15 @@ const SearchForm = ({
       <strong>Search:</strong>
     </InputWithLabel>
 
-    <StyledButtonLarge type="submit" disabled={!searchTerm}>
-      Submit
-    </StyledButtonLarge>
-  </StyledSearchForm>
-)
+    <button 
+      type="submit" 
+      disabled={!searchTerm}
+      className="button button_large"  
+    >
+      <BiSearchAlt />
+    </button>
+  </form>
+);
 
 const InputWithLabel = ({ 
   id, 
@@ -238,9 +167,11 @@ const InputWithLabel = ({
   
 return (
   <>
-    <StyledLabel htmlFor={id}>{children}</StyledLabel>
+    <label htmlFor={id} className="label">
+      {children}
+    </label>
     &nbsp;
-    <StyledInput
+    <input
       ref={inputRef}
       id={id}
       type={type}
@@ -264,22 +195,23 @@ const List = ({list, onRemoveItem}) => (
 );
 
 const Item = ({item, onRemoveItem}) => (
- <StyledItem>
-    <StyledColumn width="40%">
+ <li className="item">
+    <span style={{ width: "40%"  }}>
       <a href={item.url}>{item.title}</a>
-    </StyledColumn>
-    <StyledColumn width="30%">{item.author}</StyledColumn>
-    <StyledColumn width="10%">{item.num_comments}</StyledColumn>
-    <StyledColumn width="10%">{item.points}</StyledColumn>
-    <StyledColumn width="10%">
-      <StyledButtonSmall 
+    </span>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button
       type ="button" 
       onClick={() => onRemoveItem(item)}
+      className="button button_small"
       >
-        Dismiss
-      </StyledButtonSmall>
-    </StyledColumn>
-  </StyledItem>
+        <ImCheckmark2 />
+      </button>
+    </span>
+  </li>
 );
 
 export default App;
